34fa55ecb99c326db35a06266090de021356de8d
Chris Deal <cdeal@mymail.mines.edu>
2021-03-21 14:23:34 -0600

    Ran clang..again

--- src/pzip.c
+++ src/pzip.c
@@ -6,7 +6,7 @@
 #include "pzip.h"
 
 struct threadInfo *master;
-pthread_mutex_t fLock[26] = {0};
+pthread_mutex_t fLock[26] = { 0 };
 pthread_barrier_t theWall;
 int *returny;
 /**
@@ -39,7 +39,7 @@ void pzip(int n_threads, char *input_chars, int input_chars_size,
 
 	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
-	pthread_barrier_init(&theWall, NULL, n_threads); 
+	pthread_barrier_init(&theWall, NULL, n_threads);
 
 	pthread_t threadZ[n_threads];
 	int rc;
@@ -47,17 +47,20 @@ void pzip(int n_threads, char *input_chars, int input_chars_size,
 
 	returny = malloc(sizeof(int) * n_threads);
 
-	struct threadInfo tInfo = { n_threads, input_chars, input_chars_size, zipped_chars, zipped_chars_count, char_frequency };
+	struct threadInfo tInfo = { n_threads,		input_chars,
+				    input_chars_size,	zipped_chars,
+				    zipped_chars_count, char_frequency };
 
 	master = &tInfo;
 	for (j = 0; j < n_threads; j++) {
-		rc = pthread_create (&threadZ[j], &attr, callBack, (void *) j); 								if (rc != 0) {
+		rc = pthread_create(&threadZ[j], &attr, callBack, (void *)j);
+		if (rc != 0) {
 			perror("Error on pthread_create");
 			exit(rc);
 		}
 	}
 	if (pthread_attr_destroy(&attr) != 0) {
-	        perror("");
+		perror("");
 		exit(rc);
 	}
 	for (int i = 0; i < n_threads; i++) {
@@ -74,45 +77,46 @@ void pzip(int n_threads, char *input_chars, int input_chars_size,
 }
 
 void *callBack(void *arg)
- {
-	long tid = (long) arg;
+{
+	long tid = (long)arg;
 	int allotment = (master->size) / (master->n_threads);
-	int start = tid * allotment; 
+	int start = tid * allotment;
 	int index = 0;
 	int length = allotment + start;
 	int charCount = 1;
-	struct zipped_char *local = malloc(sizeof(struct zipped_char) * allotment); 
+	struct zipped_char *local =
+		malloc(sizeof(struct zipped_char) * allotment);
 
 	struct zipped_char zippity;
 
-	for (int i = start; i < length; i++) { 
-		 char currChar = master->input_chars[i];
-		 char nextChar = master->input_chars[i+1];
-		if (currChar == nextChar && (i < (length-1))) {
+	for (int i = start; i < length; i++) {
+		char currChar = master->input_chars[i];
+		char nextChar = master->input_chars[i + 1];
+		if (currChar == nextChar && (i < (length - 1))) {
 			charCount++;
-		}
-	       else {
+		} else {
 			zippity.character = master->input_chars[i];
 			zippity.occurence = charCount;
-      		       	local[index] = zippity;
+			local[index] = zippity;
 			pthread_mutex_lock(&fLock[local[index].character - 97]);
-			master->frequency[local[index].character - 97] += charCount;
-			pthread_mutex_unlock(&fLock[local[index].character - 97]);
+			master->frequency[local[index].character - 97] +=
+				charCount;
+			pthread_mutex_unlock(
+				&fLock[local[index].character - 97]);
 			index++;
 			charCount = 1;
 		}
-
 	}
 	returny[tid] = index;
 	pthread_barrier_wait(&theWall);
 	*master->count += index;
-	int spacer = 0; 
+	int spacer = 0;
 
 	for (int i = 0; i < tid; i++)
-		spacer += returny[i];		
-	for (int i = 0; i < index; i++) 
-		master->zippy[i+spacer] = local[i];
-	
+		spacer += returny[i];
+	for (int i = 0; i < index; i++)
+		master->zippy[i + spacer] = local[i];
+
 	free(local);
 	pthread_exit(NULL);
 }
